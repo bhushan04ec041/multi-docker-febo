c5dbf86980f211c65e5569dd36700cfb4550fb3b
bhushan04ec041 <91435043+bhushan04ec041@users.noreply.github.com>
2023-09-21 15:17:48 +0530

    Update keys.js

--- server/keys.js
+++ server/keys.js
@@ -4,10 +4,6 @@ module.exports = {
   pgUser: process.env.PGUSER,
   pgHost: process.env.PGHOST,
   pgDatabase: process.env.PGDATABASE,
-<<<<<<< HEAD
-  pgPassword: process.env. POSTGRES_PASSWORD,
-=======
   pgPassword: process.env.POSTGRES_PASSWORD,
->>>>>>> origin/main
   pgPort: process.env.PGPORT
 };
